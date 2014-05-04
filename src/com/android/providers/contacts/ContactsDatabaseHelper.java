ef71a29481099e00ac217c98011317e35cf63c0b
burstlam <burstlam@gmail.com>
2014-05-05 01:33:41 +0800

    Revert "Add one column for subscription in callLog database."
    This reverts commit 506fded15f14c566660818c8601de584ce0ae55d.
    
    Conflicts:
    	src/com/android/providers/contacts/ContactsDatabaseHelper.java

--- src/com/android/providers/contacts/ContactsDatabaseHelper.java
+++ src/com/android/providers/contacts/ContactsDatabaseHelper.java
@@ -115,7 +115,7 @@ public class ContactsDatabaseHelper extends SQLiteOpenHelper {
      *   800-899 Kitkat
      * </pre>
      */
-    static final int DATABASE_VERSION = 804;
+    static final int DATABASE_VERSION = 803;
 
     private static final String DATABASE_NAME = "contacts2.db";
     private static final String DATABASE_PRESENCE = "presence_db";
@@ -1299,8 +1299,7 @@ public class ContactsDatabaseHelper extends SQLiteOpenHelper {
                 Voicemails.MIME_TYPE + " TEXT," +
                 Voicemails.SOURCE_DATA + " TEXT," +
                 Voicemails.SOURCE_PACKAGE + " TEXT," +
-                Voicemails.STATE + " INTEGER," +
-                Calls.SUBSCRIPTION + " INTEGER NOT NULL DEFAULT 0" +
+                Voicemails.STATE + " INTEGER" +
         ");");
 
         // Voicemail source status table.
@@ -2530,11 +2529,6 @@ public class ContactsDatabaseHelper extends SQLiteOpenHelper {
             // now indexed as names.
             upgradeSearchIndex = true;
             oldVersion = 803;
-        }
-
-        if (oldVersion < 804) {
-            upgradeToVersion804(db);
-            oldVersion = 804;
         }
 
         if (upgradeViewsAndTriggers) {
@@ -4052,15 +4046,6 @@ public class ContactsDatabaseHelper extends SQLiteOpenHelper {
                 + " ADD is_restricted INTEGER;");
     }
     
-    private void upgradeToVersion804(SQLiteDatabase db) {
-        try {
-            db.execSQL("ALTER TABLE " + Tables.CALLS
-                    + " ADD " + Calls.SUBSCRIPTION + " INTEGER NOT NULL DEFAULT 0;");
-        } catch (SQLException e) {
-            Log.w(TAG, "Exception upgrading contacts2.db from 803 to 804 " + e);
-        }
-    }
-
     public String extractHandleFromEmailAddress(String email) {
         Rfc822Token[] tokens = Rfc822Tokenizer.tokenize(email);
         if (tokens.length == 0) {
